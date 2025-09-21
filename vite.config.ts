196bcd80f4bb16642872a97e0c87fcaaf049be48
Hyke60 <hyke60@yahoo.co.uk>
2025-09-21 17:11:11 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  --base: '/blindspotforspoilers/',
+  //base: '/blindspotforspoilers/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
